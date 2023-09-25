b59a242cfc9a505dc5c7a3d40880dc8c7446488a
ehteesham <ehteshaam44>
2023-09-26 03:17:49 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Random Password Generator
+
+- Clone this Repo using this command.
+```shell
+git clone git@github.com:Ehteesham/Password-Generator.git
+```
+
+- Then run this command for installing all the modules
+```shell
+pip install -r requirements.txt
+```
+
+- After done with installation run this command
+```shell
+python main.py
+```
---
+++ main.py
@@ -0,0 +1,27 @@
+import kivy
+from kivy.app import App
+# from kivy.core.window import Window
+from kivy.uix.boxlayout import BoxLayout
+
+# Window.size = (640, 700)
+kivy.require('1.9.0')
+class MyRoot(BoxLayout):
+    def __init__(self):
+        super(MyRoot, self).__init__()
+
+    def calc_symbol(self, symbol):
+        self.calc_field.text += symbol
+
+    def clear(self):
+        self.calc_field.text = ""
+    
+    def get_result(self):
+        self.calc_field.text = str(eval(self.calc_field.text))
+
+class Calculator(App):
+    def build(self):
+        return MyRoot()
+
+if __name__ == '__main__':
+    calc = Calculator()
+    calc.run()
